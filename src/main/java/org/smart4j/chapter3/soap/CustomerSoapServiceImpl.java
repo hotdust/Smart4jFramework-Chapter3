4bfdff09d40fe3808df04b11ff3e1f1f9c9e0309
shijiapeng <shijiapeng@moko.cc>
2017-01-15 20:24:08 +0800

    添加：如何访问服务的注释

--- src/main/java/org/smart4j/chapter3/soap/CustomerSoapServiceImpl.java
+++ src/main/java/org/smart4j/chapter3/soap/CustomerSoapServiceImpl.java
@@ -8,6 +8,8 @@ import org.smart4j.plugin.soap.Soap;
 
 /**
  * Created by shijiapeng on 17/1/9.
+ * 访问路径：http://localhost:8080/chapter3/soap/CustomerSoapService?wsdl
+ * 注意：上面的访问路径，是已经把Deployment的ApplicationContext设置成了"/chapter3"
  */
 @Service
 @Soap
